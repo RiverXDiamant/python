4d9b59fe62b4f35183ded097f20ccc31bca3f121
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-03-09 11:42:17 -0500

    added practice exercises for nested for loops

---
+++ Excercises/Module_3/Loops/nested_for_loops.py
@@ -0,0 +1,16 @@
+# Nested For Loops
+
+# Nested loop within a function
+for left in range(7):
+    for right in range(left, 7):
+        print("[" + str(left) + "|" + str(right) + "]", end="")
+    print()
+
+
+# Combining a nested loop with conditionals
+
+teams = ['Dragons', 'Wolves', 'Pandas', "Unicorns"]
+for home_team in teams:
+    for away_team in teams:
+        if home_team != away_team:
+            print(home_team + " vs " + away_team)
