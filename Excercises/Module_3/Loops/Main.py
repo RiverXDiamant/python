707e11ac821c56a14ea2cc984dec5a2058f6d71c
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-03-09 11:12:12 -0500

    added additional comments to practice exercises

--- Excercises/Module_3/Loops/Main.py
+++ Excercises/Module_3/Loops/Main.py
@@ -1,37 +1,37 @@
-# Loop_Practice
+# Loop_Practice: Using the range() function
 
 # ------------------
 # range(stop)
 # ------------------
 
-# for n in range(3):
-#     print(n)
+for n in range(3):
+    print(n)
 
-# for n in range(3+1):
-#     print(n)
+for n in range(3+1):
+    print(n)
 
 # ------------------
 # range(start, stop)
 # ------------------
 
-# for n in range(2, 6):
-#     print(n)
+for n in range(2, 6):
+    print(n)
 
-# for n in range(5, 10+1):
-#     print(n)
+for n in range(5, 10+1):
+    print(n)
 
 # ------------------
 # range(start,stop,step)
 # ------------------
 
-# for n in range(4, 15+1, 2):
-#     print(n)
+for n in range(4, 15+1, 2):
+    print(n)
 
-# for n in range(2*2, 25, 3+2):
-#     print(n)
+for n in range(2*2, 25, 3+2):
+    print(n)
 
-# for n in range(10, 0, -2):
-#     print(n)
+for n in range(10, 0, -2):
+    print(n)
 
 # -----------------------------------------
 # Key Takeaways
