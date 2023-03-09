db404dbdd44237f7d7ca60306dbdfeb2b50ac2a6
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-03-09 11:56:27 -0500

    addd notes and exercises

--- Excercises/Module_3/Loops/nested_for_loops.py
+++ Excercises/Module_3/Loops/nested_for_loops.py
@@ -1,5 +1,8 @@
 # Nested For Loops
 
+# ? For loops are best when you want to iterate over a known sequence of elements
+# ? When you want to operate while a certain condition is true, while loops are the best choice
+
 # Nested loop within a function
 for left in range(7):
     for right in range(left, 7):
@@ -14,3 +17,24 @@ for home_team in teams:
     for away_team in teams:
         if home_team != away_team:
             print(home_team + " vs " + away_team)
+
+# ------------------------------------------------
+
+# Iterates over the list and greets each friend
+
+
+def greet_friends(friends):
+    for friend in friends:
+        print("Hi " + friend)
+
+
+greet_friends(['Taylor', 'Luis', 'Jamaal', 'Eli'])
+
+# ------------------------------------------------
+
+# greet_friends("Barry") <-- will iterate through each letter of a string
+#! If you want it to iterate over the whole string, have that string be a part of a list
+
+# ------------------------------------------------
+
+# greet an individual friend
