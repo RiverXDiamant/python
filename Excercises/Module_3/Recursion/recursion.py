2367063fd653a82e4818f2df6771b79d3003bcb2
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-03-17 15:24:50 -0400

    added recursive function example

--- Excercises/Module_3/Recursion/recursion.py
+++ Excercises/Module_3/Recursion/recursion.py
@@ -1,1 +1,11 @@
 # Recursion: The repeated application of the same procedure to a smaller problem
+#   - lets us tackle complex problems by reducing the problem to a simpler one
+#   - a way of doing a repetitive task by having a function call itself
+#   -  Base Case ( specific condition): A recursive function calls itself usually with a modified parameter until it reaches a specific condition.
+
+# ? A recursive function will usually have this structure:
+
+def recursive_function(parameters):
+    if base_case_condition(parameters):
+        return base_case_value
+    recursive_function(modified_parameters)
