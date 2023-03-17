3f00845c1db50e014db658576c20264f29fbea53
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-03-17 15:36:56 -0400

    added example #1

--- Excercises/Module_3/Recursion/recursion.py
+++ Excercises/Module_3/Recursion/recursion.py
@@ -9,3 +9,20 @@ def recursive_function(parameters):
     if base_case_condition(parameters):
         return base_case_value
     recursive_function(modified_parameters)
+
+# Example 1: Fill in the blanks to make the is_power_of function return whether the number is a power of the given base. Note: base is assumed to be a positive number.
+
+
+def is_power_of(number, base):
+    # Base case: when number is smaller than base.
+    if number < base:
+        # If number is equal to 1, it's a power (base**0).
+        return number == 1
+
+    # Recursive case: keep dividing number by base.
+    return is_power_of(number//base, base)
+
+
+print(is_power_of(8, 2))  # Should be True
+print(is_power_of(64, 4))  # Should be True
+print(is_power_of(70, 10))  # Should be False
