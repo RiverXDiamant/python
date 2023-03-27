3dd680700346f582256caf8be8b369aedcd2e458
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-03-27 11:47:42 -0400

    added str example

--- Excercises/Module_4/Basic_Structures.py
+++ Excercises/Module_4/Basic_Structures.py
@@ -7,6 +7,14 @@
 
 # String Indexing and Slicing
 
-#! Strings in Python are immutable
+#! Strings in Python are immutable; Must create a new str based on the old one
 
 # String indexing allows you to access individual characters in a string. You can do this by using square brackets and the location, or index, of the character you want to access. It's important to remember that Python starts indexes at 0. So to access the first character in a string, you would use the index [0]. If you try to access an index that’s larger than the length of your string, you’ll get an IndexError. This is because you’re trying to access something that doesn't exist! You can also access indexes from the end of the string going towards the start of the string by using negative values. The index [-1] would access the last character of the string, and the index [-2] would access the second-to-last character.
+
+
+# Example
+
+word = "supercalifragilisticexpialadocious"
+print(word.index("x"))
+
+# Output: 21
