7dea84d6e8c8de4653382e3c89d31c714e4f4cef
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-04-03 10:47:35 -0400

    added third example

--- Excercises/Module_4/Coding_Skills.py
+++ Excercises/Module_4/Coding_Skills.py
@@ -171,3 +171,15 @@ Weather = "Rainfall"
 
 print(Weather[:4])
 # output: "Rain"
+
+
+def nametag(first_name, last_name):
+    return ("{} {}.".format(first_name, last_name[0:1]))
+
+
+print(nametag("Jane", "Smith"))
+# Should display "Jane S."
+print(nametag("Francesco", "Rinaldi"))
+# Should display "Francesco R."
+print(nametag("Jean-Luc", "Grand-Pierre"))
+# Should display "Jean-Luc G."
