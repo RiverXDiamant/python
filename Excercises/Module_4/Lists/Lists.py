c082a6defa0f6d91783a1fb041ae0d440af5629a
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-04-10 10:47:22 -0400

    added example #4

--- Excercises/Module_4/Lists/Lists.py
+++ Excercises/Module_4/Lists/Lists.py
@@ -75,10 +75,10 @@ def skip_elements(elements):
     return new_list
 
 
-# Should be ['a', 'c', 'e', 'g']
-print(skip_elements(["a", "b", "c", "d", "e", "f", "g"]))
-# Should be ['Orange', 'Strawberry', 'Peach']
-print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach']))
+# # Should be ['a', 'c', 'e', 'g']
+# print(skip_elements(["a", "b", "c", "d", "e", "f", "g"]))
+# # Should be ['Orange', 'Strawberry', 'Peach']
+# print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach']))
 
 # List Comprehensions -
 # Lets us create new lists based on sequences or ranges
@@ -92,15 +92,26 @@ print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach']))
 # More efficient way  using List comprehension
 
 multiples = [x*7 for x in range(1, 11)]
-print(multiples)
+# print(multiples)
 
 # List Comprehension #2
 # generate a list of the lengths if the strings
 
 languages = ["Python", "Perl", "Ruby", "Go", "Java", "C"]
 lengths = [len(language) for language in languages]
-print(lengths)
+# print(lengths)
 
 # All numbers divisible by 3 from 0 to 100
 z = [x for x in range(0, 101) if x % 3 == 0]
 print(z)
+
+
+def odd_numbers(n):
+    return [x for x in range(0, n+1) if x % 2 != 0]
+
+
+print(odd_numbers(5))  # Should print [1, 3, 5]
+print(odd_numbers(10))  # Should print [1, 3, 5, 7, 9]
+print(odd_numbers(11))  # Should print [1, 3, 5, 7, 9, 11]
+print(odd_numbers(1))  # Should print [1]
+print(odd_numbers(-1))  # Should print []
