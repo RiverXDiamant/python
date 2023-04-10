04540a596c3fc782c46b8ab9578fd3d285805512
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-04-10 10:01:03 -0400

    added example #2; iteration and def function

--- Excercises/Module_4/Lists/Lists.py
+++ Excercises/Module_4/Lists/Lists.py
@@ -47,3 +47,15 @@ for index, person in enumerate(winners):
 # 1 - Ashley
 # 2 - Dylan
 # 3 - Reese
+
+
+# Example # 2 - define a function that receives a list of people
+
+def full_emails(people):
+    result = []
+    for email, name in people:
+        result.append("{} <{}>".format(name, email))
+    return result
+
+
+print(full_emails([("alex@example.com", "Alex Diego")]))
