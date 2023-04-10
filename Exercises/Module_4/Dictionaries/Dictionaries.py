2416dc0ec2ac71e5f50104aa11bb8b4e960cf397
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-04-10 11:41:11 -0400

    updated dir name

---
+++ Exercises/Module_4/Dictionaries/Dictionaries.py
@@ -0,0 +1,7 @@
+# =========================== Dictionaries ==========================
+
+# dictionaries are used to organize elements into collections. Unlike lists, you don't access elements inside dictionaries using their position. Instead, the data inside dictionaries take the form of pairs of keys and values. To get a dictionary value we use its corresponding key. Another way these two vary is while in a list the index must be a number, in a dictionary you can use a bunch of different data types as keys, like strings, integers, floats, tuples, and more. The name dictionaries comes from how they work in a similar way to human language dictionaries. In an English language dictionary the word comes with a definition.
+
+x = {}
+
+print(type(x))
