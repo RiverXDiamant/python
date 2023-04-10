d631e10eeb5986edaf9e532c3c79b7c01438f3d9
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-04-10 09:34:11 -0400

    added Tuple example and notes

--- Excercises/Module_4/Lists/Lists.py
+++ Excercises/Module_4/Lists/Lists.py
@@ -14,3 +14,17 @@ print(get_word("This is a lesson about lists", 4))  # Should print: lesson
 print(get_word("This is a lesson about lists", -4))  # Nothing
 print(get_word("Now we are cooking!", 1))  # Should print: Now
 print(get_word("Now we are cooking!", 5))  # Nothing
+
+
+# ? We use tuples to store information that refers to a specific thing and does not change; i.e. - someones Full Name
+
+# Tuple Example
+
+def file_size(file_info):
+    name, type, size = file_info
+    return ("{:.2f}".format(size / 1024))
+
+
+print(file_size(('Class Assignment', 'docx', 17875)))  # Should print 17.46
+print(file_size(('Notes', 'txt', 496)))  # Should print 0.48
+print(file_size(('Program', 'py', 1239)))  # Should print 1.21
