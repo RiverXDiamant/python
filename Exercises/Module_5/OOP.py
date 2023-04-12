806c828d190915a775be4d03e7b0d1d0ddc01472
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-04-12 12:52:03 -0400

    added example #2 for inheritance

--- Exercises/Module_5/OOP.py
+++ Exercises/Module_5/OOP.py
@@ -106,3 +106,21 @@ toms = Pear("green", "sweet")
 carnelian = Grape("purple", "sweet")
 print(toms.flavor)
 print(carnelian.color)
+
+
+class Clothing:
+    material = ""
+
+    def __init__(self, name):
+        self.name = name
+
+    def checkmaterial(self):
+        print("This {} is made of {}".format(self.name, self.material))
+
+
+class Shirt(Clothing):
+    material = "Cotton"
+
+
+polo = Shirt("Polo")
+polo.checkmaterial()
