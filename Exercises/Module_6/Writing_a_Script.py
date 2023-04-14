711e0b23ef3a0f634c9de3a5d78599561bb3a722
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-04-13 21:09:00 -0400

    added Module 6 dir; project file notes

---
+++ Exercises/Module_6/Writing_a_Script.py
@@ -0,0 +1,5 @@
+# ========== Final Project Introduction: Writing a Script From the Ground Up ==========
+
+# Problem Statement
+
+#! Your manager wants to create a daily report that tracks the use of machines. Specifically, she wants to know which users are currently connected to which machines, it's your job to create the report. In your company, there's a system that collects every event that happens on the machines on the network. Among the many events collected it records each time a user logs in or out of a computer. With this information, we want to write a script that generates a report of which users are logged in to which machines at that time.
