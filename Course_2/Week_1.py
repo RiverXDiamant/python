8cdc2872c209ee6983af314bf7aee5d6fbb09575
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-06-21 23:43:40 -0400

    added pseudocode for examples

--- Course_2/Week_1.py
+++ Course_2/Week_1.py
@@ -1,1 +1,11 @@
-cat areas.py
+# Checking Health of PC Exercise
+
+
+import shutil
+import psutil
+
+
+# Define a check_disk_usage function that will receive a distant check and return true if there's more than 20 percent free or false if it's less.
+
+
+# Define a function called check_cpu_usage. Check the usage for a whole second. We'll say the machine is healthy, it a cpu_usage is less than 75 percent.
