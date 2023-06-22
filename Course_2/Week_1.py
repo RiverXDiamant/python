b2f0abc113f5b6f6bd5149f6e89a1bff58b18489
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-06-21 23:46:59 -0400

    added function to check disk usage

--- Course_2/Week_1.py
+++ Course_2/Week_1.py
@@ -7,5 +7,11 @@ import psutil
 
 # Define a check_disk_usage function that will receive a distant check and return true if there's more than 20 percent free or false if it's less.
 
+def check_disk_usage(disk):
+    du = shutil.disk_usage(disk)
+    free = du.free / du.total * 100
+    return free > 20
+
+
 
 # Define a function called check_cpu_usage. Check the usage for a whole second. We'll say the machine is healthy, it a cpu_usage is less than 75 percent.
